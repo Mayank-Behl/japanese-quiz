97d6c34559a6d56010a4b615baec23be256b1d62
Mayank-Behl <37586705+Mayank-Behl@users.noreply.github.com>
2024-01-03 00:01:23 +0530

    Change base path to deployment from local

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://Mayank-Behl.github.io/japanese-quiz/",
-  base: "https://Mayank-Behl.github.io/",
+  base: "https://Mayank-Behl.github.io/japanese-quiz/",
+  // base: "https://Mayank-Behl.github.io/",
   plugins: [react()],
 });
